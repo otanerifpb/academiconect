4018f5a8a6845aba5408aeb1fd3b2f7312adb6c0
otanerifpb <renato.ribeiro.pro@gmail.com>
2023-06-26 21:28:05 -0300

    Atualização: Dentro do AuthController foi retirado os métodos valide(), inValido() e a injeção de dependência do EstudanteRepository

--- src/main/java/br/edu/ifpb/pweb2/academiConect/controller/AuthController.java
+++ src/main/java/br/edu/ifpb/pweb2/academiConect/controller/AuthController.java
@@ -1,19 +1,19 @@
 package br.edu.ifpb.pweb2.academiConect.controller;
 
-import java.util.Optional;
+//import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import br.edu.ifpb.pweb2.academiConect.model.Estudante;
-import br.edu.ifpb.pweb2.academiConect.repository.EstudanteRepository;
-import br.edu.ifpb.pweb2.academiConect.util.PasswordUtil;
+//import br.edu.ifpb.pweb2.academiConect.model.Estudante;
+//import br.edu.ifpb.pweb2.academiConect.repository.EstudanteRepository;
+//import br.edu.ifpb.pweb2.academiConect.util.PasswordUtil;
 
 // Rota para o acesso da class AuthController
 // REQFUNC 13 - Autenticação e Autorização
@@ -21,8 +21,8 @@ import br.edu.ifpb.pweb2.academiConect.util.PasswordUtil;
 @RequestMapping("/auth")
 public class AuthController {
 
-    @Autowired
-    private EstudanteRepository estudanteRepository;
+    // @Autowired
+    // private EstudanteRepository estudanteRepository;
 
     // Rota para o acessar com uso do GET
     // Cria um novo objeto conhecido como "cookie", para identificar o Usuário
@@ -34,19 +34,19 @@ public class AuthController {
     }
     
     // Rota para o acessar com uso do POST, para validar Usuário e Senha
-    @RequestMapping(value="/valide", method = RequestMethod.POST)
-    public ModelAndView valide(Estudante estudante, HttpSession session, ModelAndView mav,
-            RedirectAttributes redAttrs) {
-        if ((estudante = this.isValido(estudante)) != null) {
-            session.setAttribute("usuario", estudante);
-            mav.addObject("succesMensagem", estudante.getNome() + " Bem Vindo ao ACADEMICONECT");
-            mav.setViewName("redirect:/home");
-        } else {
-            redAttrs.addFlashAttribute("errorMensagem", "Login e/ou Senha inválidos!");
-            mav.setViewName("redirect:/auth");
-        }
-        return mav;
-    }
+    // @RequestMapping(value="/valide", method = RequestMethod.POST)
+    // public ModelAndView valide(Estudante estudante, HttpSession session, ModelAndView mav,
+    //         RedirectAttributes redAttrs) {
+    //     if ((estudante = this.isValido(estudante)) != null) {
+    //         session.setAttribute("usuario", estudante);
+    //         mav.addObject("succesMensagem", estudante.getNome() + " Bem Vindo ao ACADEMICONECT");
+    //         mav.setViewName("redirect:/home");
+    //     } else {
+    //         redAttrs.addFlashAttribute("errorMensagem", "Login e/ou Senha inválidos!");
+    //         mav.setViewName("redirect:/auth");
+    //     }
+    //     return mav;
+    // }
 
     // Rota para o sair da Sessão
     @RequestMapping("/logout")
@@ -57,16 +57,16 @@ public class AuthController {
     }
 
     // Método para validação do Usuário e Senha
-    private Estudante isValido(Estudante estudante) {
-        Optional<Estudante> opestudanteSGBD = estudanteRepository.findByEmail(estudante.getEmail());
-        //Estudante estudanteSGBD = estudanteRepository.findByEmail(estudante.getEmail());
-        Estudante estudanteBD = opestudanteSGBD.get();
-        boolean valido = false;
-        if (opestudanteSGBD.isPresent()) {
-            if (PasswordUtil.checkPass(estudante.getSenha(), estudanteBD.getSenha())) {
-                valido = true;
-            }
-        }
-        return valido ? estudanteBD : null;
-    }
+    // private Estudante isValido(Estudante estudante) {
+    //     Optional<Estudante> opestudanteSGBD = estudanteRepository.findByEmail(estudante.getEmail());
+    //     //Estudante estudanteSGBD = estudanteRepository.findByEmail(estudante.getEmail());
+    //     Estudante estudanteBD = opestudanteSGBD.get();
+    //     boolean valido = false;
+    //     if (opestudanteSGBD.isPresent()) {
+    //         if (PasswordUtil.checkPass(estudante.getSenha(), estudanteBD.getSenha())) {
+    //             valido = true;
+    //         }
+    //     }
+    //     return valido ? estudanteBD : null;
+    // }
 }
